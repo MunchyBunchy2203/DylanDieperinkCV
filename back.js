6e8cca35ff278562fb225be5c1a89d045b344062
Snorre <dieperinkdylan@gmail.com>
2022-08-19 18:25:42 -0400

    Form Styling Update

--- back.js
+++ back.js
@@ -16,3 +16,15 @@ $(document).ready(function () {
         }
     });
 });
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+    label.innerHTML = label.innerText
+    .split("")
+    .map(
+        (letter, idx) =>
+        '<span style="transition-delay:${idx * 50}ms">${letter}</span>'
+    )
+    .join("");
+});
