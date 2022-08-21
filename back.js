b1eac9dd0225c591b25f52a4664953ec01a83c7a
Snorre <dieperinkdylan@gmail.com>
2022-08-21 12:17:33 -0400

    Hamburger menu added

--- back.js
+++ back.js
@@ -31,3 +31,9 @@ labels.forEach((label) => {
 
 var userEmail = document.getElementById("sEmail");
 var userName = document.getElementById("sName");
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
